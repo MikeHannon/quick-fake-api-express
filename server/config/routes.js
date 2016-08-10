422cdc9cd8941baea3ac606305cf620a43834040
Sara Wong <1sarabyte@gmail.com>
2016-08-10 07:21:17 -0700

    allow puts in CORS

--- server/config/routes.js
+++ server/config/routes.js
@@ -5,6 +5,7 @@ var Users = require('./../controllers/users.js');
 module.exports = (function(app){
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
